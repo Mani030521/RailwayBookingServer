2eceecc5d4db2e5f100b118d837e25d40c28bea9
Manikandan Arjunan <M1047144@mindtree.com>
2020-05-24 20:23:10 +0530

    first heroku commit

---
+++ API's/index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const adminRoutes = require("./Admin/Routes");
+const userRoutes = require("./User/Routes");
+const ticketRoutes = require("./Ticketing/Routes");
+const trainRoutes = require("./Train/Routes");
+
+const app = express();
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "*");
+    res.header("Access-Control-Allow-Methods", "*");
+    next();
+});
+
+app.use(bodyParser.json());
+
+app.use("/", (req, res) => {
+    res.send(" Welcome to Railway booking service ");
+});
+app.use("/admin", adminRoutes);
+app.use("/user", userRoutes);
+app.use("/trains", trainRoutes);
+app.use("/ticketing", ticketRoutes);
+
+module.exports = app;
